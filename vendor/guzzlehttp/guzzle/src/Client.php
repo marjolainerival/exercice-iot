811abbeef54e0ad3399da7dd1632271012f747b8
marjolainerival <rival.marjolaine@gmail.com>
2017-02-09 17:46:59 +0100

    modif

--- vendor/guzzlehttp/guzzle/src/Client.php
+++ vendor/guzzlehttp/guzzle/src/Client.php
@@ -62,7 +62,7 @@ class Client implements ClientInterface
     public function __construct(array $config = [])
     {
         if (!isset($config['handler'])) {
-            $config['handler'] = Guzzle\src\HandlerStack::create();
+            $config['handler'] = HandlerStack::create();
         }
 
         // Convert the base_uri to a UriInterface
